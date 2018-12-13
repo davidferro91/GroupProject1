37ccd0ca7e5413068403a12adeb72e81aa5ac4db
Julia Magyar <magyar.juliar@gmail.com>
2018-12-13 18:48:19 -0500

    mostly working?

--- assets/javascript/user-input.js
+++ assets/javascript/user-input.js
@@ -32,19 +32,46 @@ $("#submit-button").on("click", function(event){
     date = $("#date-input").val();
     //get lat/long from user location
     locationInput = $("#location-input").val().trim();
-    console.log(locationInput)
-    //parse int of value user entered and check if number
-    locationCheck = parseInt(locationInput);
+    //check if input is a number
+    locationCheck = $.isNumeric(locationInput);
     //if entry is not zipcode convert to zipcode
-    if (locationCheck = NaN){
-            
-
+    if (locationCheck == false){
+        //need to split city from state 
+        console.log("not number")
+        var locationSplit = locationInput.split(",")
+        var city = locationSplit[0].trim();
+        var state = locationSplit[1].trim();
+        //get zipcode for city and state
+        var apiKey = "l9q0mOZpkOQJ2keHRlEisVi8HOMgGRkkPZKSGhHh3n5OqSVRRUzVqDs42RvVbkjE"
+        var queryURL = "https://www.zipcodeapi.com/rest/" + apiKey + "/city-zips.json/"+ city + "/" + state;
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+            .then(function(response){
+                console.log(response)
+                //grab zipcode here
+            })
+    }
+    else{
+        zipcode = locationInput;
+        console.log(zipcode)
     }
     //get lat/long from zipcode
-
+    var apiKey = "l9q0mOZpkOQJ2keHRlEisVi8HOMgGRkkPZKSGhHh3n5OqSVRRUzVqDs42RvVbkjE"
+    var queryURL = "https://www.zipcodeapi.com/rest/"+ apiKey + "/info.json/"+ zipcode + "/degrees";
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    })
+        .then(function(response){
+            console.log(response)
+        })
     
     
 })
+
+
 
 
 } //end of onload
