d513f0f75a5df227b1449877201719cffbc86466
Julia Magyar <magyar.juliar@gmail.com>
2018-12-13 21:32:05 -0500

    state dropdown added

--- assets/javascript/user-input.js
+++ assets/javascript/user-input.js
@@ -6,6 +6,13 @@ var zipcode;
 var date = moment().format("YYYY-MM-DD");
 $("#date-input").attr("value", date)
 
+//create dropdown options for state box
+var states = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"];
+console.log(states.length)
+for(i = 0; i < states.length; i++){
+    opt = "<option>" + states[i] + "</option>"
+    $("#state-input").append(opt);
+}
 
 //get user location via geolocation and stores longitude and latitude, this is called on load
     //function that does everything if browser supports geolocation
@@ -14,7 +21,7 @@ $("#date-input").attr("value", date)
             navigator.geolocation.getCurrentPosition(showPosition);
         }
         else{
-            console.log("error: geolocation no available");
+            console.log("error: geolocation not available");
         }
     }
     //function called within getLocation that stores latitude and longitude
