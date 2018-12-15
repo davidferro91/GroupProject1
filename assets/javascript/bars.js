3751ffdedd4fc47355df0efba6d04e9811afdb69
davidferro <david-ferro@sbcglobal.net>
2018-12-15 11:17:34 -0500

    rounding images

--- assets/javascript/bars.js
+++ assets/javascript/bars.js
@@ -1,9 +1,3 @@
-//$.getJSON("https://maps.googleapis.com/maps/api/js?key=AIzaSyDIOdzXh8_8Z4Jj1zqdz7p7tn87J8Adf_c&bars=places&callback=initAutocomplete", function(data) {
-    //console.log(data);
-
-//}
-//);
-
     var latitude = 41.50;
     var longitude = -81.61;
     var date = "2018-12-22";
@@ -11,7 +5,7 @@
 
     function barsGenerator (response, i) {
     var barsHolder =  $("<div>");
-    barsHolder.addClass("row m-2 p-4");
+    barsHolder.addClass("row m-2 p-4 rounded");
     barsHolder.attr("id", "bars");
     var barsBox = $("<div>");
     barsBox.addClass("col");
@@ -20,10 +14,7 @@
     var barsName = response.businesses[i].name;
     nameHolder.text(barsName);
     barsBox.append(nameHolder);
-    // var timeHolder = $("<p>");
-    // var barsTime = response.bars[i].datetime_local;
-    // timeHolder.text(barsTime);
-    // barsBox.append(timeHolder);
+    
 
     var phoneHolder = $("<p>");
     var phone = response.businesses[i].display_phone;
@@ -34,7 +25,9 @@
     barImageHolder.attr("src", response.businesses[i].image_url);
     barImageHolder.attr("id", "bar-image");
     barImageHolder.attr("alt", response.businesses[i].name);
+    barImageHolder.addClass("rounded");
     barsBox.append(barImageHolder);
+
     var url = $("<a>");
     url.addClass("row p-3");
     url.attr("target", "_blank");
@@ -45,11 +38,6 @@
     return barsHolder;
     
     }
-
-
-// function barsGenerator (response, i) {
-//     var term = $("bars");
-// }
 
 
 var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=bars&latitude=" + latitude + "&longitude=" + longitude;
